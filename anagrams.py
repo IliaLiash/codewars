52ba70d0ec79e5609f8da2d0689839df28a5ed4c
Ilia LIash <70135336+IliaLiash@users.noreply.github.com>
2021-05-18 07:51:16 +0300

    Update anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -4,3 +4,6 @@ def anagrams(word, words):
         if (len(word) == len(element)) and (set(word) == set(element)):
             res.append(element)
     return res
+
+test.assert_equals(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'])
+test.assert_equals(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])
